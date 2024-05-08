17013979a61eee46b51d08001f87f99f1b37e2ce
rahimihamid <rahimi501@gmail.con>
2024-05-08 15:04:09 +0330

    edit_settings

--- eshop_project/settings.py
+++ eshop_project/settings.py
@@ -142,9 +142,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static/',
 ]
-STATIC_ROOT = [
-    BASE_DIR / 'staticfiles'
-]
+STATIC_ROOT = BASE_DIR / 'staticfiles/'
+    
 
 # Media files (picture, audio, video, document,...)
 MEDIA_URL = 'media/'
